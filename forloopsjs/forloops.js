6fcf767b47ed9d6c71e8c2ce3c7b3958ffe1d14d
WinterCricket <hodhodhod2003@gmail.com>
2017-03-19 13:03:14 -0500

    add curly braces

--- forloopsjs/forloops.js
+++ forloopsjs/forloops.js
@@ -4,22 +4,25 @@ for(var i = -10; i <= 19; i++){
 }
 
 console.log("print even numbers from 10 to 40");
-for(var i = 10; i <= 40; i++)
+for(var i = 10; i <= 40; i++){
 	if( i % 2 === 0){
 		console.log(i);
 	}
+}
 
 console.log("print odd numbers from 300 to 333");
-for(var i = 300; i <= 333; i++)
+for(var i = 300; i <= 333; i++){
 	if(i % 2 !== 0){
 		console.log(i);
 	}
+}
 
 console.log("print numbers divisable by 5 and 3 from 5 to 50");
-for(var i = 5; i <= 50; i++)
+for(var i = 5; i <= 50; i++){
 	if(i % 5 === 0 && i % 3 === 0){
 		console.log(i);
 	}
+}
 
 // In The Hospital
 
@@ -28,4 +31,4 @@ for(var i = 5; i <= 50; i++)
 	//speaker says someone 
 	//somewhere at sometime 
 	//tapped keys, he 
-	//sleeps less lonely
+	//sleeps near someone
