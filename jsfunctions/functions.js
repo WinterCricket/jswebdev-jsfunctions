656a79c0ea1bdb814275280e842a9192d54a89a4
WinterCricket <hodhodhod2003@gmail.com>
2017-03-21 07:42:36 -0500

    prepare functions

---
+++ jsfunctions/functions.js
@@ -0,0 +1,49 @@
+
+// function capitalize(str){
+// 	if(typeof str === "number"){
+// 		return str + " isn't a string!"
+// 	}
+// 	return str.charAt(0).toUpperCase() + str.slice(1);
+// } 
+
+// var city = "london";
+// var capital = capitalize(city);
+
+// console.log(capital);
+
+// var num = 87;
+// var capital = capitalize(num);
+
+// console.log(capital);
+
+// function isEven(num){
+// 	if(num % 2 === 0){
+// 		return true;
+// 	}else{
+// 		return false;
+// 	}
+// }
+
+function isEven(num){
+	return num % 2 === 0;
+}
+
+// function factorial(num) {
+//   if (num < 0) 
+//         return -1;
+//   else if (num == 0) 
+//       return 1;
+//   else {
+//       return (num * factorial(num - 1));
+//   }
+// }
+
+function factorial(num){
+
+	var result = 1;
+
+	for(var i = 2; i <= num; i++){
+		result *= i;
+	}
+	return result;
+}
